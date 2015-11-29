fe5892500ca358dc96153cc0737d86acb3d204fe
matt <fitz.matt.d@gmail.com>
2015-11-29 22:01:36 +0100

    add base62 module

---
+++ base62.py
@@ -0,0 +1,24 @@
+BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+BASE = len(BASE62)
+
+
+def encode(num, numerals=BASE62):
+    if num == 0:
+        return numerals[0]
+    # add handling for negative numbers. maybe
+    result = ''
+    while num:
+        result = numerals[num % BASE] + result
+        num = num // BASE
+    return result
+
+
+def decode(s, numerals=BASE62):
+    strlen = len(s)
+    num = 0
+    idx = 0
+    for char in s:
+        power = strlen - (idx + 1)
+        num += numerals.index(char) * BASE ** power
+        idx += 1
+    return num
